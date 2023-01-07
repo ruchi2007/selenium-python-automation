e6e91e2e5a3db8868a06e2af0071986a2c3b558c
ruchi2007 <ruchi2007usa@gmail.com>
2023-01-06 22:08:46 -0500

    Initial commit

---
+++ test/test_login.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+import time
+import pytest
+
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    driver.get(url)
+    driver.implicitly_wait(1000)
+
+    yield
+    driver.close()
+    driver.quit()
+
+
+def test_login_successfully(test_setup):
+    username = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"
+    password = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"
+    login = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"
+    driver.find_element_by_xpath(username).send_keys("Admin")
+    driver.find_element_by_xpath(password).send_keys("admin123")
+    driver.find_element_by_xpath(login).click()
+    expected_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    actual_url = driver.current_url
+    driver.save_screenshot("image.png")
+    assert expected_url == actual_url
+    print(driver.current_url)
+    title = driver.title
+    assert title == "OrangeHRM"
+
+def test_verify_user_should_not_login_with_invalid_credential(test_setup):
+    username = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"
+    password = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"
+    login = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"
+    driver.find_element_by_xpath(username).send_keys("abc")
+    driver.find_element_by_xpath(password).send_keys("admin123")
+    driver.find_element_by_xpath(login).click()
+    error_messege = driver.find_element_by_xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p").text
+    assert "Invalid credentials" in error_messege
+
+# def test_closebrowser():
+#     driver.close()
+#     driver.quit()
---
+++ test/test_login_test.py
@@ -0,0 +1,38 @@
+import pytest
+
+from pages.homePage import HomePage
+from pages.loginPage import LoginPage
+from selenium import webdriver
+import time
+
+from util import usercredential
+
+
+@pytest.mark.usefixtures('setup')
+class TestLogin:
+    @pytest.mark.smoke
+
+    def test_verify_user_should_login_successfully(self):
+        driver = self.driver
+        lp = LoginPage(driver)
+        lp.enter_username(usercredential.username)
+        lp.enter_password(usercredential.password)
+        lp.click_submit_btn()
+        print("\n tc1..............>success")
+    @pytest.mark.smoke
+
+    def test_verify_user_should_logout_successfully(self):
+        driver = self.driver
+        hp = HomePage(driver)
+        hp.click_logout_icon()
+        print("\n tc2............>success")
+
+    @pytest.mark.regrassion
+    def test_login_verify_error_message_for_invalid_credential(self):
+        driver = self.driver
+        lp = LoginPage(driver)
+        lp.enter_username(usercredential.inv_username)
+        lp.enter_password(usercredential.inv_password)
+        lp.click_submit_btn()
+        assert "Invalid credentials" in lp.error_text_message()
+        print("\n tc3..............>success")
